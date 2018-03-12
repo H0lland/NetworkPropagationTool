7fccffa0ba0183433cff259c56dcf0cc6dfd4329
H0lland <matiturner@gmail.com>
2018-03-12 11:25:30 -0400

    Added documentation to functions

--- src/main/java/catworks/Network.java
+++ src/main/java/catworks/Network.java
@@ -4,23 +4,40 @@ public class Network extends AbstractNetwork {
 
     private static int count;
     private int networkID;
+    ArrayList<ArrayList<int>> matix;
 
     public Network() {
         networkID = count++;
     }
 
+    /**
+    * [addNode description]
+    */
     public void addNode() {
         return; // TODO: Implement.
     }
-
-    public void deleteNode(int nodeID) {
+    /**
+    * [deleteNode description]
+    * @param long nodeId [description]
+    */
+    public void deleteNode(long nodeID) {
         return; // TODO: Implement method.
     }
 
+    /**
+    * [addEdge description]
+    * @param long source [description]
+    * @param long dest [description]
+    */
     public void addEdge(long source, long dest) {
         return; // TODO: Implement method.
     }
 
+    /**
+    * [deleteEdge description]
+    * @param long source [description]
+    * @param long dest [description]
+    */
     public void deleteEdge(long source, long dest) {
         return; // TODO: Implement method.
     }
@@ -28,4 +45,5 @@ public class Network extends AbstractNetwork {
     // Accessor and mutator methods.
     public int getID() { return networkID; }
     public int getCount() { return count; }
+    public ArrayList<ArrayList<int>> getMatrix() {return matrix; }
 }
