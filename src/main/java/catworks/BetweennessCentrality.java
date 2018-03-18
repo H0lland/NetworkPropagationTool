6f9a77ee6e8d314e8fa28fcb9d9123ee15f78a01
Asare Nkansah <aknk222@uky.edu>
2018-03-18 17:43:05 -0400

    Creation of Betweenness Centrality class
    I created the class to implement betweenness centrality for the network.

---
+++ src/main/java/catworks/BetweennessCentrality.java
@@ -0,0 +1,20 @@
+package catworks;
+
+public class BetweennessCentrality implements Centrality {
+
+    /**
+     * [getCentralities description]
+     * @param  Integer[][] matrix        [description]
+     * @return             [description]
+     */
+    public Integer[] getCentralities(Integer[][] matrix) {
+        Integer[] centralities = {0};
+
+        // for node in net:
+        //     for other_nodes in net:
+        //         ...
+
+        return centralities;
+    }
+
+}
